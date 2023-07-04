031aacc908b50bb3031468eb3a0a7a7ed0b7f4d0
kyle.cartmell <kyle.cartmell@engineering.digital.dwp.gov.uk>
2023-07-04 20:06:55 +0100

    feat(exception): updates ticket service to process, validate request and make payment and reserve seats

--- src/pairtest/TicketService.js
+++ src/pairtest/TicketService.js
@@ -1,12 +1,51 @@
-import TicketTypeRequest from './lib/TicketTypeRequest.js';
-import InvalidPurchaseException from './lib/InvalidPurchaseException.js';
+import TicketPaymentService from '../thirdparty/paymentgateway/TicketPaymentService.js';
+import SeatReservationService from '../thirdparty/seatbooking/SeatReservationService.js';
+import RequestProcessor from './lib/RequestProcessor.js';
+import RequestValidator from './lib/RequestValidator.js';
 
 export default class TicketService {
   /**
    * Should only have private methods other than the one below.
    */
+  #paymentService = new TicketPaymentService();
+
+  #seatReservationService = new SeatReservationService();
+
+  #requestValidator = new RequestValidator();
+
+  #totalNumberOfTickets = 0;
+
+  #totalCost = 0;
+
+  #totalNumberOfSeats = 0;
+
+  #tickets = {
+    ADULT: 0,
+    CHILD: 0,
+    INFANT: 0,
+  };
 
   purchaseTickets(accountId, ...ticketTypeRequests) {
-    // throws InvalidPurchaseException
+    ticketTypeRequests.forEach((request) => {
+      const {
+        type, numberOfTickets, cost, seats,
+      } = RequestProcessor.processRequest(request);
+      this.#tickets[type] += numberOfTickets;
+      this.#totalNumberOfTickets += numberOfTickets;
+      this.#totalCost += cost;
+      this.#totalNumberOfSeats += seats;
+    });
+
+    RequestValidator.validateRequest(accountId, this.#totalNumberOfTickets, this.#tickets.ADULT);
+
+    this.#paymentService.makePayment(accountId, this.#totalCost);
+    this.#seatReservationService.reserveSeat(accountId, this.#totalNumberOfSeats);
+
+    return {
+      numberOfSeats: this.#totalNumberOfSeats,
+      totalCost: this.#totalCost,
+      tickets: this.#tickets,
+      totalNumberOfTickets: this.#totalNumberOfTickets,
+    };
   }
 }
